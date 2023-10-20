19692ac4334d376efed701917655484f3998da57
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2023-10-20 18:35:50 +0300

    feat: deploy project

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/store";
+import { REPO_NAME } from "./constants/repo";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <App />
       </BrowserRouter>
     </Provider>
