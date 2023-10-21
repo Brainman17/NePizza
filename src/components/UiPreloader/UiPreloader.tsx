3c02f52af768ee91a6f9369146fd379abbab7f0e
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2023-10-21 23:06:30 +0300

    deploy

--- src/components/UiPreloader/UiPreloader.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./UiPreloader.module.scss";
-import Preloader from "../../static/cube.svg";
-
-const UiPreloader = () => {
-  return <img className={styles.loader} src={Preloader} alt="Preloader" />;
-};
-
-export default UiPreloader;
--- src/constants/api.ts
+++
@@ -1,1 +0,0 @@
-export const BASEURL: string = "https://65212399a4199548356cd797.mockapi.io/items";
